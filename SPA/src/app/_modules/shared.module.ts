b1d757e410186f0ea036716660132bd6a3ee20c6
JoshueSolisE <120432290+JoshueSolisE@users.noreply.github.com>
2024-02-17 15:05:12 -0600

    Parcial-01 Added features in description
    Commit_01
    Added member-edit component
    Updated member-edit component
    Added NgForm and @ViewChild
    Added API support to persist user edition
    Added loading indicator/service
    Storing the state of the app via service

--- SPA/src/app/_modules/shared.module.ts
+++ SPA/src/app/_modules/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxSpinner, NgxSpinnerModule } from 'ngx-spinner';
 
 
 @NgModule({
@@ -16,11 +17,15 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
       preventDuplicates: true,
       progressBar: true
     }),
+    NgxSpinnerModule.forRoot({
+      type: 'square-loader'
+    }),
   ],
   exports: [
     BsDropdownModule,
     ToastrModule,
-    TabsModule
+    TabsModule,
+    NgxSpinnerModule
   ]
 })
 export class SharedModule { }
