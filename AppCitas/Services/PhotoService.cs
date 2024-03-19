583895b9ee62e5d2e4098fc5a8c144c07b41a9cf
JoshueSolisE <120432290+JoshueSolisE@users.noreply.github.com>
2024-03-19 13:02:28 -0600

    Added photo service in API

--- AppCitas/Services/PhotoService.cs
+++ AppCitas/Services/PhotoService.cs
@@ -5,13 +5,42 @@ namespace AppCitas.Services;
 
 public class PhotoService : IPhotoService
 {
+    private readonly Cloudinary _cloudinary;
+    public PhotoService(Ioptions<CloudinarySettings> config)
+    {
+        var acc = new Account
+        (
+            config.Value.CloudName,
+            config.Value.ApiKey,
+            config.Value.ApiSecret
+        );
+
+        _cloudinary = new Cloudinary(acc);
+    }
+
     public Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
     {
-        throw new NotImplementedException();
+        var uploadResult = new ImageUploadResult();
+        if (file.lenght > 0)
+        {
+            using var stream = file.OpenReadStream();
+            var uploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(file.FileName, stream),
+                Transformation = new Transformation()
+                                    .Height(500).Width(500)
+                                    .Crop("fill").Gravity("face"),
+                Folder = "AppCitas-n8"
+            };
+            uploadResult = await _cloudinary.UploadAsync(uploadParams);
+        }
+        return uploadResult;
     }
 
     public Task<DeletionResult> DeletePhotoAsync(string publicId)
     {
-        throw new NotImplementedException();
+        var deleteParams = new DeletionParams(publicId);
+        
+        return await _cloudinary.DestroyAsync(deleteParams);
     }
 }
